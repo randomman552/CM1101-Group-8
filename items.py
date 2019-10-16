a86fbb49874fd587f8518d16060e6309bd058c80
George Grainger <GraingerGY@cardiff.ac.uk>
2019-10-16 12:48:14 +0100

    Map added

--- items.py
+++ items.py
@@ -1,12 +1,11 @@
-item_example = {
+item_medication = {
     #id of the room, is used as its name
-    "id": "example",
+    "id": "medication",
     #Name is shown when listing items from inventory
-    "name": "an example item",
+    "name": "your medication",
     #Description can be read with inspect command
     "description":
-    """This is an example item.
-    I wonder what it does.""",
+    """This is your medication""",
     #Weight is used with the inventory weight system, this can just be set to 0 if you dont want to use it
     "weight": 0
 }
@@ -15,5 +14,5 @@ item_example = {
 #Item must be put in this list to be usable.
 #Item should be put in like this: "example": item_example
 items = {
-    "example": item_example
+    "medication": item_medication
 }
--- map.py
+++ map.py
@@ -1,23 +1,97 @@
 from items import *
 
-room_example = {
+room_bedroom = {
     #The name of the room, this is shown on screen when the player is in the room.
-    "name": "Example",
+    "name": "Bedroom",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is an example room.""",
+    """This is your bedroom""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
-    "exits": {},
+    "exits": {"north": "Bathroom", "west": "Hallway"},
     #Items contains a list of the items available in the room on game start.
-    "items": [item_example],
-    #Win conditions is used to contain a list of win conditions associated with the room (work in progress).
-    "win conditions": {
-        "items": []
-    }
+    "items": []
+}
+
+room_bathroom = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Bathroom",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is your bathroom""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"south": "Bedroom"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
+}
+
+room_hallway = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Hallway",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is the hallway""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"east": "Bedroom", "west": "Lounge", "north": "Kitchen", "south": "Outside"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
+}
+
+room_living_room = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Lounge",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is the living room""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"east": "Hallway", "north": "Office"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
+}
+
+room_office = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Office",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is your office""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"south": "Lounge"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
+}
+
+room_kitchen = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Kitchen",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is the kitchen""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"south": "Hallway"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
+}
+
+room_outside = {
+    #The name of the room, this is shown on screen when the player is in the room.
+    "name": "Outside",
+    #Description of the room, this is shown on screen when the player is in the room.   
+    "description":
+    """This is outside?""",
+    #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
+    "exits": {"north": "Hallway"},
+    #Items contains a list of the items available in the room on game start.
+    "items": []
 }
 
 #Contains all of the rooms in the game with an identifier.
 #For example, "Start": room_example.
 rooms = {
-    "Start": room_example
+    "Bathroom": room_bathroom,
+    "Bedroom": room_bedroom,
+    "Hallway": room_hallway,
+    "Lounge": room_living_room,
+    "Office": room_office,
+    "Kitchen": room_kitchen
+    "Outside": room_outside
 }

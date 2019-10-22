2eb0ec77b9cdaf498fc28f938a4f193daa24c140
George Grainger <GraingerGY@cardiff.ac.uk>
2019-10-22 15:30:09 +0100

    Addded pickup sound

--- sound.py
+++ sound.py
@@ -3,11 +3,16 @@ from pygame import mixer
 mixer.pre_init(44100, 16, 2, 4096)
 mixer.init()
 exit_sound = mixer.Sound(file="Sounds\exit_sound.wav")
+pickup_sound = mixer.Sound(file="Sounds\Drop.wav")
 mixer.music.load("Sounds\BG_Music.wav")
 
 def play_exit_sound():
     if not mixer.get_busy():
         exit_sound.play()
+
+def play_pickup_sound():
+    if not mixer.get_busy():
+        pickup_sound.play()
 
 def BG_Music(play = True):
     if play:

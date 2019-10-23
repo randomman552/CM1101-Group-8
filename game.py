b97a31fc6ba647a982ebc77d347aea5e74b375d4
Sean Berry <berrys5@cardiff.ac.uk>
2019-10-23 12:03:44 +0100

    Updated mirror function

--- game.py
+++ game.py
@@ -331,7 +331,7 @@ def print_mirror_menu():
         
 def print_appearance():
     global player
-    print("Your name is " + player["description"]["name"] + " and you are " + player["description"]["height"] + ". You have "
+    print("Your name is " + player["description"]["name"] + " and you are a " + player["description"]["height"] +  " tall " + player["description"]["gender"] + ". You have "
     + player["description"]["hair_length"] + " " + player["description"]["hair_colour"] + " hair with " + player["description"]["eye_colour"] + " eyes.")
     
 def check_appearance():
@@ -359,10 +359,10 @@ def print_list(lists, command):
     global player
     counter = 0
     for item in lists:
-        print("[" + str(counter + 1) + "] " + item)
+        print(" [" + str(counter + 1) + "] " + item)
         counter += 1
         
-    print("[" + str(counter + 1) + "] keep it the same")
+    print(" [" + str(counter + 1) + "] keep it the same")
     i = str(input())
     s = ""
     for ch in i:
@@ -371,6 +371,7 @@ def print_list(lists, command):
         
     if s != str(counter):
         player["description"][command] = lists(s)
+        print("Your " + command + " has been changed.")
     
 def password_prompt():
     

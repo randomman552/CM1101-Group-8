9f537ac771a47f762f580c7092e29e6418e36c3a
Radu-Ionel Stanicel <stanicelr@cardiff.ac.uk>
2019-10-23 01:56:28 +0100

    Update map.py

--- map.py
+++ map.py
@@ -5,12 +5,15 @@ room_bedroom = {
     "name": "Bedroom",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is your bedroom""",
+    """In a large cozy room with not too many items you
+    only find a bed, a nightstand and a wardrobe. There is
+    a big window near your bed. A dim light comes from
+    outsidebut soon it dissappears.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"north": "Bathroom", "west": "Hallway"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_mirror,item_window]
+    "items default": [item_window, item_wardrobe, item_matches, item_candle, item_alarm_clock]
 }
 
 room_bathroom = {
@@ -18,12 +21,16 @@ room_bathroom = {
     "name": "Bathroom",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is your bathroom""",
+    """As you open the door you see a dim light. It's just
+    a reflection. You can only approach the mirror as you
+    cannot see anything else... .The mirror is placed on
+    a drawer that you cannot open. There is a black window
+    near the sink.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"south": "Bedroom"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_medication,item_window]
+    "items default": [item_mirror, item_writing_on_wall, item_medicine]
 }
 
 room_hallway = {
@@ -31,12 +38,13 @@ room_hallway = {
     "name": "Hallway",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is the hallway""",
+    """Long and empty with an old wood floor, the hallway leads you to other two
+    rooms: the kitchen and the living room. Hopefully the exit is near.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"east": "Bedroom", "west": "Lounge", "north": "Kitchen", "south": "Outside"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_coat]
+    "items default": [item_coat, item_fusebox, item_vase, item_door]
 }
 
 room_living_room = {
@@ -44,12 +52,14 @@ room_living_room = {
     "name": "Lounge",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is the living room""",
+    """It looks like nothing you saw untill now. You can see your reflection in
+    the marble floor. Near an armchair there is a table with a fruit bowl on it.
+    The fruits are fresh but the air has a rotten smell.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"east": "Hallway", "north": "Office"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_family_picture,item_window]
+    "items default": [item_picture, item_fuse, item_wall_markings, item_magnifying_glass]
 }
 
 room_office = {
@@ -57,12 +67,13 @@ room_office = {
     "name": "Office",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is your office""",
+    """A short, cold breeze washes the rotten smell away. You can see a blue light
+    on a computer screen. The window is oppened but you see nothing but a sea shore filled with weird objects.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"south": "Lounge"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_book,item_window]
+    "items default": [item_book, item_computer, item_paper]
 }
 
 room_kitchen = {
@@ -70,12 +81,13 @@ room_kitchen = {
     "name": "Kitchen",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is the kitchen""",
+    """Is this childhood? Old products on the shelf, same cutlery from 30 years ago.
+    Is this a dream or a nightmare? Nothing makes sense.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"south": "Hallway"},
     #Items contains a list of the items available in the room on game start.
     "items": [],
-    "items default": [item_knife,item_window]
+    "items default": [item_knife, item_bowl_of_noodles, item_painting]
 }
 
 room_outside = {
@@ -83,7 +95,7 @@ room_outside = {
     "name": "Outside",
     #Description of the room, this is shown on screen when the player is in the room.   
     "description":
-    """This is outside?""",
+    """Finally I'm out! On a field. The house dissappeared.""",
     #Exits contains the available exits from the room, this must be set in order for the player to leave the room.
     "exits": {"north": "Hallway"},
     #Items contains a list of the items available in the room on game start.

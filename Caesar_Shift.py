c14ba2058403cdbea8355556c3b998ac8badcaf9
George Grainger <GraingerGY@cardiff.ac.uk>
2019-10-23 22:28:47 +0100

    Caesar_shift move into items only place where used

--- Caesar_Shift.py
+++
@@ -1,5 +0,0 @@
-def caesar_cipher(text, shift):
-  characters = [char for char in text]
-  return ''.join(list(map(lambda character: 
-    ' ' if ord(character) == 32 else chr((ord(character) + shift - 97) % 26 + 97),
-    characters)))
